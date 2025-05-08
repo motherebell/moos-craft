cf1fd07364b224a8e790ed6bd244a6293d4a7eae
can420nabis <48324513+motherebell@users.noreply.github.com>
2025-05-08 19:19:29 +0200

    Delete Game.gd

--- Game.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-func _ready():
-    print("Spiel gestartet - Android Touch-Version")
-
-func _unhandled_input(event):
-    if event is InputEventScreenTouch and event.pressed:
-        print("Berührt bei Position: ", event.position)
